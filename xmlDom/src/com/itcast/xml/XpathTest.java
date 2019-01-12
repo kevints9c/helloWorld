dc0c294979e9031c5abaf119cf4b72553e9ab22a
kevints9c <229034392@qq.com>
2019-01-12 23:06:48 +0800

    study commit
    Change-Id: Ia4d583d045e81065f80ac01da890dbb2dbff080b
    Signed-off-by: kevints9c <229034392@qq.com>

---
+++ xmlDom/src/com/itcast/xml/XpathTest.java
@@ -0,0 +1,53 @@
+package com.itcast.xml;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+public class XpathTest {
+
+	public static void main(String[] args) {
+
+		/*try {
+			SAXReader reader = new SAXReader();
+			Document document = reader.read(new File("src/xml/desElement.xml"));
+			Element rootElement = document.getRootElement();
+			Element nameElement = (Element) rootElement.selectSingleNode("//name");
+//			System.out.println(nameElement.getText());
+			List<Element> list = rootElement.selectNodes("//name");
+			for (Element element : list) {
+				System.out.println(element.getText());
+			}
+		} catch (DocumentException e) {
+			
+			e.printStackTrace();
+		}*/
+		
+		try {
+			SAXReader reader = new SAXReader();
+			Document document = reader.read(new File("src/xml/desElement.xml"));
+			
+			Element rootElement = document.getRootElement();
+			Element name=(Element) rootElement.selectSingleNode("//name");
+			System.out.println(name.getText());
+			
+			List<Element> names = rootElement.selectNodes("//name");
+			for (Element element : names) {
+				
+				System.out.println(element.getText());
+			}
+			
+		} catch (DocumentException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
